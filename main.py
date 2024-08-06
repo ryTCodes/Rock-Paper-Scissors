adb7f9ac49fc632311669b1f1143f4fe5adf9c13
ryTCodes <tlkrayyanps@gmail.com>
2024-08-06 20:58:04 +0530

    Update commit

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
 import random
 from arts import rock, paper, scissors
 
-game_choices = [rock, paper, scissors]
 
-print("Welcome to Rock, Paper and Scissors!!")
-user_choice = int(input("1. Rock\n2. Paper\n3. Scissor\nEnter your Choice: "))
+def main():
+    print("Welcome to Rock, Paper and Scissors!!")
+    play_game()
 
-computer_choice = random.randint(1, 3)
 
-print(f"\nYour Choice:-\n{game_choices[user_choice - 1]}")
-print(f"Computer Choose:-\n{game_choices[computer_choice - 1]}")
+def play_game():
+    user_choice = int(input("1. Rock\n2. Paper\n3. Scissor\nEnter your Choice: "))
+    computer_choice = random.randint(1, 3)
 
-if user_choice > 3 or user_choice < 1:
-    print("Invalid Input!")
-elif computer_choice == 1 and user_choice == 3:
-    print("You Lose")
-elif user_choice == 1 and computer_choice == 3:
-    print("You Won")
-elif user_choice > computer_choice:
-    print("You Won")
-elif computer_choice > user_choice:
-    print("You Lose")
-elif user_choice == computer_choice:
-    print("It's a draw")
+    if user_choice > 3 or user_choice < 1:
+        print("Invalid Input!")
+        return
+    else:
+        print(f"\nYour Choice:-\n{game_choices[user_choice - 1]}")
+        print(f"Computer Choose:-\n{game_choices[computer_choice - 1]}")
+
+    if user_choice == computer_choice:
+        print("It's a draw")
+    elif computer_choice == 1 and user_choice == 3:
+        print("You Lose")
+    elif user_choice == 1 and computer_choice == 3:
+        print("You Won")
+    elif user_choice > computer_choice:
+        print("You Won")
+    elif computer_choice > user_choice:
+        print("You Lose")
+
+    should_continue = input("\nEnter 'yes' to play again, 'no' to exit: ").strip().lower()
+    if should_continue == 'yes':
+        play_game()
+
+
+if __name__ == "__main__":
+    game_choices = [rock, paper, scissors]
+    main()
